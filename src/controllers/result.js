4c15d0fb13dd2131bb8fbd697afb0bc500d56def
sisso19 <sigurthor12@ru.is>
2014-03-18 01:44:29 +0000

    Búið að breyta þannig að new template er komið í sér file til að koma í veg fyrir lagg, hægt er að skoða result úr spurningum

--- src/controllers/result.js
+++ src/controllers/result.js
@@ -1,23 +1,75 @@
 app.controller("ResultController", ["$http", "$scope", "UserFactory", "$routeParams", "$window", "$location",
 	function($http, $scope, UserFactory, $routeParams, $window, $location) {
-		alert("in dat ctrl");
 
 		$scope.admin = UserFactory.isAdmin();
 		$scope.view = true;
+		$scope.noResult = false;
+		$scope.routeParm = $routeParams.resultID;
 		if($window.sessionStorage.token !== undefined) {
-			if($routeParams.templateID === undefined) {
+			
+			if($routeParams.resultID === undefined) {
 				
-				$scope.say = function(mella){
-					alert(mella);
-				}
+				$http.get('http://localhost:19358/api/v1/evaluations').then(function(respond) {
+					$scope.evalClosed = [];
+					$scope.evalOpen = [];
+					$scope.evalNew = [];
+					console.log(respond.data);
+					if(respond.data !== undefined) {
+						respond.data.forEach(function(entry) {
+							if(entry.Status === "closed") {
+								$scope.evalClosed.push(entry);
+							} 
+							else if(entry.Status === "open") {
+								$scope.evalOpen.push(entry)
+							}
+							else {
+								$scope.evalNew.push(entry)
+							}
+						})
+					}
+				});
 			}
 			else{
+				
 				$scope.view = false;
+				var answers = 0;
+				$http.get('http://localhost:19358/api/v1/evaluations/' + $routeParams.resultID).then(function(respond) {
+					$scope.evalAns = respond.data;
+					console.log(respond.data);
+					if($scope.evalAns.Questions !== undefined) {
+						$scope.showTextQuestions = [];
+						for(var i = 0; i < $scope.evalAns.Questions.length; i++) {
+									$scope.showTextQuestions[i] = false;
+								}
+						/*$scope.evalAns.Questions.forEach(function(entry) {
+							
 
+							if(entry.Type !== "text"){
+								answers = 0;
+								for(var i =0; i < entry.Result.length; i++) {
+									answers = answers + entry.Result[i].Count;
+								}
+								entry.totalAnswers = answers;
+								for(var i =0; i < entry.Result.length; i++) {
+									entry.Result[i].;
+								}
+							}
+							else {
+								entry.totalAnswers = 0;
+							}
+						})*/
+					}
+					else{
+						$scope.noResult = true;
+					}
+					
+				});
 			}
 		}
 		else {
+			
 			alert("rdrctg u!");
 			$location.path("/");
 		}
+
 	}]);
--- src/controllers/template.js
+++ src/controllers/template.js
@@ -2,7 +2,6 @@ app.controller("TemplateController", ["$http", "$scope", "UserFactory", "$routeP
 	function($http, $scope, UserFactory, $routeParams, $window, $location) {
 		$scope.admin = UserFactory.isAdmin();
 		$scope.view = true;
-		$scope.new = false;
 		$scope.templShow = [];
 		$scope.evaluations = [];
 		$scope.dates = [];
@@ -25,9 +24,8 @@ app.controller("TemplateController", ["$http", "$scope", "UserFactory", "$routeP
 					};
 				});
 			}
-			else if($routeParams.templateID === "new"){
+			else if($routeParams.templateID === "new") {
 				$scope.view = false;
-				$scope.new = true;
 				$scope.evaluation = {
 					TitleIS: "",
 					TitleEN: "",
@@ -36,6 +34,25 @@ app.controller("TemplateController", ["$http", "$scope", "UserFactory", "$routeP
 					CourseQuestions: [],
 					TeacherQuestions: []
 				};
+/*
+				$scope.evaluation.CourseQuestions.push({
+					ID: $scope.evaluation.CourseQuestions.length,
+					TextIS: "Spurning",
+					TextEN: "Question",
+					ImageURL: "",
+					Type: "single",
+					Answers: []
+				});
+
+				$scope.evaluation.CourseQuestions.Answers.push({
+							ID: $scope.evaluation.CourseQuestions.Answers.length,
+							TextIS: "Svar",
+							TextEN: "Answer",
+							ImageURL: "",
+							Weight: 0
+						});
+*/
+				console.log($scope.evaluation);
 			}
 			else {
 				$scope.view = false;
@@ -70,13 +87,12 @@ app.controller("TemplateController", ["$http", "$scope", "UserFactory", "$routeP
 			}
 
 			$scope.submitTemplate = function() {
-				console.log("the template submitted:");
-				console.log($scope.evaluation);
+				$scope.evaluation.IntroTextEn = "Hello";
+				$scope.evaluation.TitleEN = "Hola";
 				$http.post('http://localhost:19358/api/v1/evaluationtemplates', $scope.evaluation).then(function(respond) {
 					if(respond.status === 204)
 					{
-						console.log("Template was Created TODO:message");
-						console.log(respond);
+						alert("Template Was Created Successfully!");
 						$location.path("/templates");
 					}
 					else {
@@ -120,7 +136,7 @@ app.controller("TemplateController", ["$http", "$scope", "UserFactory", "$routeP
 			}
 
 			$scope.newTemplate = function() {
-				$location.path("/templates/new");
+				$location.path("/templates/n/new");
 			}
 		}
 		else{
